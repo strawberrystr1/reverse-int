26f4bafae3f88a90c8bbeac23f44921ee373ece9
strawberrystr1 <andrey_overdose@mail.ru>
2021-02-01 19:37:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+    return Math.sign(n) > 0 ? Number(n.toString().split('').reverse().join('')) : Number(n.toString().split('').splice(1).reverse().join(''))
 }
